7576f8a65d04273f337d7ad356ba0f46ac3380c8
emil <emilbabazade29@gmail.com>
2021-02-24 15:13:10 +0200

    change anectode to a different one randomly

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+interface AppProps {
+  anectodes: string[]
+}
+
+const App = ({anectodes}: AppProps) => {
+  const [selected, setSelected] = useState(0)
+
+  const onChangeAnectode = () => {
+    const newIndex = Math.floor(Math.random() * Math.floor(anecdotes.length))
+    setSelected(newIndex)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <button onClick={onChangeAnectode}>Change Anectode</button>
+    </div>
+  )
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anectodes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
